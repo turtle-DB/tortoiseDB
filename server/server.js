9fec96f83d768a95510e13256b2811c55df4ad0b
Max Appleton <maxiappleton@gmail.com>
2018-07-17 21:49:15 -0500

    refactor mongo code

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { tortoiseDB } = require('../db/tortoiseDB');
 
 const app = express();
+
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-// This gets one object
 app.get('/store/:id', (req, res) => {
   tortoiseDB.read(req.params.id)
     .then(doc => res.send(doc));
@@ -16,11 +16,13 @@ app.route('/store')
   .get((req, res) => {
     tortoiseDB.readAll()
       .then(docs => res.send(docs))
-      .catch(err => console.log("readAll error:", err))
+      .catch(err => {
+        console.log("readAll error:", err);
+      })
   })
   .post((req, res) => {
     tortoiseDB.create(req.body)
-      .then(() => res.send())
+      .then((result) => res.send(result));
   })
 
 //Node env object's production port or local 3000
