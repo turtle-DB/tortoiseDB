ec82b541b873217eb1979f92260d3dea0827022d
houstonfloyd <andrew.houstonfloyd#gmail.com>
2018-08-01 12:09:36 -0400

    refactor tree merging and conflict code to process docs in batches, instead of individually

--- db/syncFrom.js
+++ db/syncFrom.js
@@ -20,7 +20,7 @@ class SyncFrom {
         if (!tortoiseSyncFromLatestHistory) {
           return 0;
         } else {
-          // If last keys don't match, just start from 0
+          // If last keys don't match, just start from 0createNewMetaDoc
           if (tortoiseSyncFromLatestHistory.lastKey !== turtleSyncToLatestHistory.lastKey) {
             return 0;
           } else {
@@ -52,59 +52,154 @@ class SyncFrom {
   }
 
 
-  findAllMissingLeafNodes(turtleMetaDocs) {
-    log(`\n\t --- Begin revision tree merge and conflict identification for ${turtleMetaDocs.length} metadocs --- `);
-    // returns a list of all turtle leaf nodes that tortoise doesn't have
-    const missingLeafNodes = [];
-
-    const promises = turtleMetaDocs.map(turtleMetaDoc => {
-      return mongoShell.command(mongoShell._meta, "READ", { _id: turtleMetaDoc._id })
-        .then(tortoiseMetaDocArr => {
-          let tortoiseMetaDoc = tortoiseMetaDocArr[0];
-          //console.log('tortoise metadoc:', tortoiseMetaDoc);
-
-          if (tortoiseMetaDoc) {
-            const newMetaDoc = this.createNewMetaDoc(tortoiseMetaDoc, turtleMetaDoc);
-            console.log('new metadoc after merge:', newMetaDoc);
-            return this.findMissingLeafNodesOfDoc(newMetaDoc)
-              .then(idRevs => {
-                console.log('leaf nodes that are missing from tortoise:', idRevs);
-                missingLeafNodes.push(...idRevs);
-                // update existing metaDoc
-                return mongoShell.command(mongoShell._meta, "UPDATE", newMetaDoc);
-              });
-          } else {
-            // if we recieve a document with one branch that has been deleted, ignore it
-            if (turtleMetaDoc._winningRev) {
-              missingLeafNodes.push(turtleMetaDoc._id + '::' + turtleMetaDoc._winningRev);
-              // insert turtleMetaDoc
-              return mongoShell.command(mongoShell._meta, "CREATE", turtleMetaDoc);
-            }
-          }
-        })
-    });
-
-    return Promise.all(promises).then(() => {
-      log(`\n\t --- Complete revision tree merge and conflict identification for ${turtleMetaDocs.length} metadocs --- `);
+  dummyFindAllMissingLeafNodes(turtleMetaDocs) {
+    return this.createMetaDocPairs(turtleMetaDocs)
+    .then((metaDocPairs) => this.dummyCreateNewMetaDocs(metaDocPairs))
+    .then((metaDocTrios) => this.dummyFindMissingLeafNodes(metaDocTrios))
+    .then((missingLeafNodes) => {
       return missingLeafNodes;
     });
   }
 
-  createNewMetaDoc(tortoiseMetaDoc, turtleMetaDoc) {
-    const tortoiseRevTree = tortoiseMetaDoc._revisions;
-    const turtleRevTree = turtleMetaDoc._revisions;
-    // console.log('tortoiseRevTree', JSON.stringify(tortoiseRevTree, undefined, 2));
-    // console.log('turtleRevTree', JSON.stringify(turtleRevTree, undefined, 2));
-    const mergedRevTree = this.mergeRevTrees(tortoiseRevTree, turtleRevTree);
-    // console.log('mergedRevTree', JSON.stringify(mergedRevTree, undefined, 2));
+  createMetaDocPairs(turtleMetaDocs) {
+    let metaDocPairs = {};
 
-    return {
-      _id: tortoiseMetaDoc._id,
-      _revisions: mergedRevTree,
-      _winningRev: this.getWinningRev(mergedRevTree),
-      _leafRevs: this.collectActiveLeafRevs(mergedRevTree)
-    };
+    const metadocPairPromises = turtleMetaDocs.map(turtleMetaDoc => {
+      return mongoShell.command(mongoShell._meta, "READ", { _id: turtleMetaDoc._id })
+      .then(tortoiseMetaDocArr => {
+        let tortoiseMetaDoc = tortoiseMetaDocArr[0];
+        metaDocPairs[turtleMetaDoc._id] = { 'turtle': turtleMetaDoc, 'tortoise': tortoiseMetaDoc };
+      })
+    });
+
+    return Promise.all(metadocPairPromises).then(() => {
+      return metaDocPairs;
+    });
   }
+
+  dummyCreateNewMetaDocs(metaDocPairs) {
+    this.dummyNewRevisionTrees(metaDocPairs);
+    this.dummyNewMetaDocs(metaDocPairs);
+    return metaDocPairs;
+  }
+
+  dummyNewRevisionTrees(metaDocPairs) {
+    let docIDs = Object.keys(metaDocPairs);
+
+    docIDs.forEach(id => {
+      let metaDocPair = metaDocPairs[id];
+      if (metaDocPair.tortoise) {
+        const tortoiseRevTree = metaDocPair.tortoise._revisions;
+        const turtleRevTree = metaDocPair.turtle._revisions;
+        metaDocPair.newRevisionTree = this.mergeRevTrees(tortoiseRevTree, turtleRevTree);
+        } else {
+        metaDocPair.new = null;
+      }
+    });
+  }
+
+  dummyNewMetaDocs(metaDocPairs) {
+    let docIDs = Object.keys(metaDocPairs);
+
+    docIDs.forEach(id => {
+      let metaDocPair = metaDocPairs[id];
+      if (metaDocPair.tortoise) {
+        metaDocPair.new = {
+          _id: metaDocPair.tortoise._id,
+          _revisions: metaDocPair.newRevisionTree,
+          _winningRev: this.getWinningRev(metaDocPair.newRevisionTree),
+          _leafRevs: this.collectActiveLeafRevs(metaDocPair.newRevisionTree)
+        };
+      } else {
+        metaDocPair.new = null;
+      }
+    });
+  }
+
+  dummyFindMissingLeafNodes(metaDocTrios) {
+    let docIDs = Object.keys(metaDocTrios);
+    let missingLeafNodes = [];
+
+    const metadocTrioPromises = docIDs.map(id => {
+      let metaDocTrio = metaDocTrios[id];
+      let newMetaDoc = metaDocTrio.new;
+      if (metaDocTrio.new) {
+        return this.findMissingLeafNodesOfDoc(newMetaDoc)
+          .then(idRevs => {
+            //console.log('leaf nodes that are missing from tortoise:', idRevs);
+            missingLeafNodes.push(...idRevs);
+            // update existing metaDoc
+            return mongoShell.command(mongoShell._meta, "UPDATE", newMetaDoc);
+          });
+      } else {
+        let turtleMetaDoc = metaDocTrio.turtle;
+        // if we recieve a document with one branch that has been deleted, ignore it
+        if (turtleMetaDoc._winningRev) {
+          missingLeafNodes.push(turtleMetaDoc._id + '::' + turtleMetaDoc._winningRev);
+          // insert turtleMetaDoc
+          return mongoShell.command(mongoShell._meta, "CREATE", turtleMetaDoc);
+        }
+      }
+    });
+
+    return Promise.all(metadocTrioPromises).then(() => {
+      return missingLeafNodes;
+    });
+  }
+
+  // findAllMissingLeafNodes(turtleMetaDocs) {
+  //   log(`\n\t --- Begin revision tree merge and conflict identification for ${turtleMetaDocs.length} metadocs --- `);
+  //   // returns a list of all turtle leaf nodes that tortoise doesn't have
+  //   const missingLeafNodes = [];
+  //
+  //   const promises = turtleMetaDocs.map(turtleMetaDoc => {
+  //     return mongoShell.command(mongoShell._meta, "READ", { _id: turtleMetaDoc._id })
+  //       .then(tortoiseMetaDocArr => {
+  //         let tortoiseMetaDoc = tortoiseMetaDocArr[0];
+  //         //console.log('tortoise metadoc:', tortoiseMetaDoc);
+  //
+  //         if (tortoiseMetaDoc) {
+  //           const newMetaDoc = this.createNewMetaDoc(tortoiseMetaDoc, turtleMetaDoc);
+  //           //console.log('new metadoc after merge:', newMetaDoc);
+  //           return this.findMissingLeafNodesOfDoc(newMetaDoc)
+  //             .then(idRevs => {
+  //               //console.log('leaf nodes that are missing from tortoise:', idRevs);
+  //               missingLeafNodes.push(...idRevs);
+  //               // update existing metaDoc
+  //               return mongoShell.command(mongoShell._meta, "UPDATE", newMetaDoc);
+  //             });
+  //         } else {
+  //           // if we recieve a document with one branch that has been deleted, ignore it
+  //           if (turtleMetaDoc._winningRev) {
+  //             missingLeafNodes.push(turtleMetaDoc._id + '::' + turtleMetaDoc._winningRev);
+  //             // insert turtleMetaDoc
+  //             return mongoShell.command(mongoShell._meta, "CREATE", turtleMetaDoc);
+  //           }
+  //         }
+  //       })
+  //   });
+  //
+  //   return Promise.all(promises).then(() => {
+  //     log(`\n\t --- Complete revision tree merge and conflict identification for ${turtleMetaDocs.length} metadocs --- `);
+  //     return missingLeafNodes;
+  //   });
+  // }
+  //
+  // createNewMetaDoc(tortoiseMetaDoc, turtleMetaDoc) {
+  //   const tortoiseRevTree = tortoiseMetaDoc._revisions;
+  //   const turtleRevTree = turtleMetaDoc._revisions;
+  //   // console.log('tortoiseRevTree', JSON.stringify(tortoiseRevTree, undefined, 2));
+  //   // console.log('turtleRevTree', JSON.stringify(turtleRevTree, undefined, 2));
+  //   const mergedRevTree = this.mergeRevTrees(tortoiseRevTree, turtleRevTree);
+  //   // console.log('mergedRevTree', JSON.stringify(mergedRevTree, undefined, 2));
+  //
+  //   return {
+  //     _id: tortoiseMetaDoc._id,
+  //     _revisions: mergedRevTree,
+  //     _winningRev: this.getWinningRev(mergedRevTree),
+  //     _leafRevs: this.collectActiveLeafRevs(mergedRevTree)
+  //   };
+  // }
 
   mergeRevTrees(node1, node2) {
     const node1Children = node1[2];
--- server/routes/syncFromRoutes.js
+++ server/routes/syncFromRoutes.js
@@ -11,36 +11,36 @@ router.post('/_last_tortoise_key', (req, res) => {
   // Initialize new replicateFrom object
   tortoiseDB.syncFrom();
   logFrom('\n\n ------- NEW Turtle ==> Tortoise SYNC ------');
-  log('\n #1 HTTP <== from Turtle requesting checkpoint from last sync session');
+  log('\n #1 HTTP POST request <== Turtle requesting checkpoint from last sync session');
 
   // Then begin replication process
   tortoiseDB.syncFromSession.getLastTortoiseKey(req.body)
   .then(lastKey => {
     log(`\n Get last Turtle key (${lastKey}) from previous sync session`);
-    log('\n #2 HTTP ==> to Turtle with last key');
+    log('\n #2 HTTP response ==> Turtle with last key');
     res.send(lastKey.toString())
   })
   .catch(err => console.log("_last_tortoise_key error:", err))
 });
 
 router.post('/_missing_rev_ids', (req, res) => {
-  log(`\n #3 HTTP <== from Turtle with (${req.body.metaDocs.length}) changed revision trees`);
-  tortoiseDB.syncFromSession.findAllMissingLeafNodes(req.body.metaDocs)
+  log(`\n #3 HTTP POST request <== Turtle with (${req.body.metaDocs.length}) changed revision trees`);
+  tortoiseDB.syncFromSession.dummyFindAllMissingLeafNodes(req.body.metaDocs)
     .then(missingRevIds => {
       //log('\n Merge revision trees and list all missing records');
-      log(`\n #4 HTTP ==> to Turtle requesting (${missingRevIds.length}) missing records`);
+      log(`\n #4 HTTP response ==> Turtle requesting (${missingRevIds.length}) missing records`);
       res.send(missingRevIds)
     })
     .catch(err => console.log("_missing_rev_ids route error:", err));
 });
 
 router.post('/_insert_docs', (req, res) => {
-  log('\n #5 HTTP <== from Turtle with missing records and updated sync history');
+  log('\n #5 HTTP POST request <== Turtle with missing records and updated sync history');
   tortoiseDB.syncFromSession.insertNewDocsIntoStore(req.body.docs)
   .then(() => log('\n Insert missing records into MongoDB'))
   .then(() => tortoiseDB.syncFromSession.updateSyncFromTurtleDoc(req.body.newSyncToTortoiseDoc))
   .then(() => log('\n Update sync history doc'))
-  .then(() => log('\n #6 HTTP ==> to Turtle with confirmation of complete sync'))
+  .then(() => log('\n #6 HTTP response ==> Turtle with confirmation of complete sync'))
   .then(() => res.send("Insert docs received"))
   .then(() => logFrom('\n ------- Turtle ==> Tortoise sync complete ------ '))
   .catch(err => console.log("_insert_docs error:", err));
