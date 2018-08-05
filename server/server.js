6e605b5a088d69a5f2bbeccbfd79d42251a6eee6
Max Appleton <maxiappleton@gmail.com>
2018-08-05 14:38:17 -0500

    expand payload limit in express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { tortoiseDB } = require('../db/tortoiseDB');
 
 const syncToRoutes = require('./routes/syncToRoutes');
@@ -6,7 +7,10 @@ const syncFromRoutes = require('./routes/syncFromRoutes');
 
 const app = express();
 
-app.use(express.json());
+// Tell the bodyparser middleware to accept more data
+app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
